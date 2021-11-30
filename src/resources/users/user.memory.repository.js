6677049f21905027c6f3aff3ac537ce5e021312e
halaveika <halaveika1988@gmail.com>
2021-11-30 03:06:25 +0300

    feat: add users store

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,43 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+
+const { v4: uuidv4 } = require('uuid');
+ 
+const userStore = [
+ {
+   id: uuidv4(),
+   name: 'Alex',
+   login: 'Alex123',
+   password: 'uhyags8d924yhhA'
+ },
+ {
+   id: uuidv4(),
+   name: 'Olya',
+   login: 'Olya123',
+   password: 'ugsgs892f4yhhA'
+ },
+ {
+   id: uuidv4(),
+   name: 'Petya',
+   login: 'Petya123',
+   password: 'uhfffffgyags8924yhhA'
+ },
+ {
+   id: uuidv4(),
+   name: 'Vasya',
+   login: 'Vasya123',
+   password: 'uhyags8924yhhA'
+ },
+ {
+   id: uuidv4(),
+   name: 'Zhora',
+   login: 'Zhora123',
+   password: 'uhffsfyags8924yhhA'
+ }
+]
+
+
+
+
+
+const getAll = async () => userStore;
 
 module.exports = { getAll };
