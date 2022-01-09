4d637289b9c075bedbe9cc312ee3ee1aa5408c80
halaveika <halaveika1988@gmail.com>
2022-01-09 23:21:12 +0300

    add: rsschool network for containers

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import logger from './common/logger';
 const start = async () => {
   try {
     await app.listen(config.PORT, config.BACKEND_HOST, () =>
-    logger.info(`App is running on ${config.BACKEND_HOST}: ${config.PORT} and logger level: ${config.LOG_LEVEL} in ${config.NODE_ENV} mode`)
+    logger.info(`App is running on ${config.BACKEND_HOST}: ${config.PORT} and logger level: ${config.LOG_LEVEL}`)
   )
   } catch (error) {
     logger.error(error)
