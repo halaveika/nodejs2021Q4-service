1a53f76b9656c163d959da8e3df32152f3cc1fba
Aliaksandr Halaveika <aliaksandr_halaveika@epam.com>
2022-02-06 23:50:48 +0300

    feat: add common dinamic swagger document

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,7 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import multipart from 'fastify-multipart';
-import fastifySwagger from 'fastify-swagger';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as path from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -24,29 +22,17 @@ async function bootstrap() {
       new FastifyAdapter(),
     );
     (app as NestFastifyApplication).register(multipart);
-    (app as NestFastifyApplication).register(fastifySwagger, {
-      mode: 'static',
-      exposeRoute: true,
-      routePrefix: '/doc',
-      specification: {
-        path: path.join(__dirname, '../doc/api.yaml'),
-        postProcessor(swaggerObject) {
-          return swaggerObject;
-        },
-        baseDir: '/doc',
-      },
-    });
   } else {
     app = await NestFactory.create(AppModule);
-    const config = new DocumentBuilder()
-      .setTitle('Trello killer')
-      .setDescription('Docment REST API')
-      .setVersion('1.0.0')
-      .addTag('halaveika')
-      .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('/doc', app, document);
   }
+  const config = new DocumentBuilder()
+  .setTitle('Trello killer')
+  .setDescription('Docment REST API')
+  .setVersion('1.0.0')
+  .addTag('halaveika')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('/doc', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
